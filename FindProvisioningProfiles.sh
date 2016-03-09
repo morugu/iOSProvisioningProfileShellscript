dc03257bc06b702a9c697fa3c4903e363c059ec3
shoya <12c1055@gmail.com>
2016-03-09 23:37:20 +0900

    Add FindProvisioningProfiles.sh

---
+++ FindProvisioningProfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$1" = "" ]
+then
+    echo "Usage: sh $0 <Provisioning Profile name>"
+    exit
+fi
+
+PROFILE_NAME=$1
+echo ${PROFILE_NAME}
+cd ~/Library/MobileDevice/Provisioning\ Profiles/
+find . -name "*.mobileprovision" | xargs grep "${PROFILE_NAME}" -l
